440b729a7b62e647056baf17e0d9b64b1c08bdfb
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2021-10-13 01:42:58 +0700

    finish Search screen

--- App.js
+++ App.js
@@ -7,39 +7,40 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Suspense} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  //SafeAreaView,
+  // ScrollView,
+  // StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  ActivityIndicator,
+  // Text,
+  // useColorScheme,
   View,
 } from 'react-native';
 // import Login from './src/components/Authentication/Login/Login';
 // import Register from './src/components/Authentication/Register/Register';
 // import ForgetPassword from './src/components/Authentication/ForgetPassword/ForgetPassword';
 // import NotifyForgetPassword from './src/components/Authentication/ForgetPassword/NotifyForgetPassword';
-import Home from './src/components/Main/Home/Home';
+// import Home from './src/components/Main/Home/Home';
 // import Menu from './src/components/Main/Menu/Menu';
 // import Profile from './src/components/AccountManagement/Profile/Profile';
 // import Setting from './src/components/AccountManagement/Setting/Setting';
-// import Search from './src/components/Main/Tutor/Search/Search';
+import Search from './src/components/Main/Tutor/Search/Search';
 ////import {Tag, TagActive, WelcomeButton} from './src/components/_common/FlexibleButton/FlexibleButton';
 //import ListMessage from './src/components/Main/Message/ListMessage';
 
 const App = () => {
-   return (
-       //<Menu></Menu>
-       <Home></Home>
-      // <ListMessage/>
-      //<Profile></Profile>
-      //<Setting></Setting>
-      //<Search></Search>
-      //<Multicheck></Multicheck>
-   );
-}
+  return (
+    //<Menu></Menu>
+    //<Home></Home>
+    // <ListMessage/>
+    //<Profile></Profile>
+    //<Setting></Setting>
+    <Search></Search>
+    //<Multicheck></Multicheck>
+  );
+};
 const styles = StyleSheet.create({
   // sectionContainer: {
   //   marginTop: 32,
